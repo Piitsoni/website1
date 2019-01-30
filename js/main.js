d1df84ab838581a6f4b1a4bdcdaad4e14f084047
Atte Pesonen <atte.pesonen@gmail.com>
2019-01-30 19:36:08 +0200

    added smoothscroll

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-window.smoothScroll = function(button) {
-    var scrollContainer = boxes;
-    do { //find scroll container
-        scrollContainer = box.boxes;
-        if (!scrollContainer) return;
-        box.scrollTop += 1;
-    } while (box.scrollTop == 0);
-
-    var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
-
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 2000);
+});
